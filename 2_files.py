8390654c47985896e28430c1ab72a5776c9ef673
Artem Khismatulln <a.khismatulli@gmail.com>
2020-09-17 13:26:34 +0500

    Выполнил задание с файлами, создан файл referat2.txt

--- 2_files.py
+++ 2_files.py
@@ -12,11 +12,22 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open('referat.txt', 'r', encoding ='utf8' ) as f:
+        with open('referat2.txt', 'w', encoding='utf8') as f2:
+           content = f.read()
+           print(len(content))
+           print(len(content.split(' ')))
+           change_marks = content.replace('.', '!')
+           f2.write(change_marks)
+           
+        
+
+
+
+
+
+        
+
 
 if __name__ == "__main__":
     main()
